489a1d5fab6a502b479e740c7e8114414f512fe8
Your Name <you@example.com>
2023-05-24 18:38:22 +0700

    chuyentrang hoanghamobile thanh index

--- src/main/java/com/java5/Asm/Controller/HomeController.java
+++ src/main/java/com/java5/Asm/Controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/login")
+    @GetMapping("hoanghamobile")
     public String test(){
-        return "admin/login";
+        return "product/index";
     }
 }
