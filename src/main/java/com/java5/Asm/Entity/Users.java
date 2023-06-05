e3e142cec8a374f79bbff303cfd58040f784faae
dangchikhanh <61544957+dangchikhanh@users.noreply.github.com>
2023-06-05 17:08:02 +0700

    Khanh sửa lại

--- src/main/java/com/java5/Asm/Entity/Users.java
+++ src/main/java/com/java5/Asm/Entity/Users.java
@@ -1,6 +1,5 @@
 package com.java5.Asm.Entity;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,27 +13,20 @@ import java.util.Collection;
 @Setter
 @NoArgsConstructor
 public class Users {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_user;
-    private String firstName;
-    private String lastName;
-    @NaturalId(mutable = true)
-    private String email;
-    private String password;
-    private boolean isEnabled = false;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id_user"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id_role"))
-    private Collection<Role> roles;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_user;
+	private String firstName;
+	private String lastName;
+	@NaturalId(mutable = true)
+	private String email;
+	private String password;
+	private boolean isEnabled = false;
 
-    public Users(String firstName, String lastName, String email,
-                String password, Collection<Role> roles) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-    }
+	public Users(String firstName, String lastName, String email, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
 }
